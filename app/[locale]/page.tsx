a5423c0f34f5bb4c1c9653b6759390b48e492e95
Joanna <jlapa.transport@gmail.com>
2024-01-03 13:08:23 +0100

    fixed empty jsons, accept-language package installed

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -14,9 +14,6 @@ export default async function Page({ params: { lng } }: {
   return (
     <>
       <h1>Hi there!</h1>
-      <Link href={`/${lng}/second-page`}>
-        second page
-      </Link>
     </>
   )
 }
