a966465afc79fcde182d06de4a1d17fa6fa071da
Joanna <jlapa.transport@gmail.com>
2024-01-03 12:25:25 +0100

    i18n reorganisation

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,9 +1,22 @@
-import Image from 'next/image'
+import Link from 'next/link'
+import { Trans } from 'react-i18next/TransWithoutContext'
+import { languages, fallbackLng } from '../i18n/settings'
+import { useTranslation } from '../i18n'
 
-export default function Home() {
+export default async function Page({ params: { lng } }: {
+  params: {
+    lng: string;
+  };
+}) {
+  if (languages.indexOf(lng) < 0) lng = fallbackLng
+  const { t } = await useTranslation(lng)
+
   return (
-    <main>
-     <h1>Hello World</h1>
-    </main>
+    <>
+      <h1>Hi there!</h1>
+      <Link href={`/${lng}/second-page`}>
+        second page
+      </Link>
+    </>
   )
 }
