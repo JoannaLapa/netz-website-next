f19d3767486c419f78e1a94f1a84f67d28e2afd5
Joanna <jlapa.transport@gmail.com>
2024-01-08 14:46:53 +0100

    fallbackNS added

--- app/i18n/settings.ts
+++ app/i18n/settings.ts
@@ -2,6 +2,7 @@ export const fallbackLng = 'de'
 export const languages = [fallbackLng, 'pl', 'en']
 export const defaultNS = 'translation'
 export const cookieName = 'i18next'
+export const fallbackNS = defaultNS
 
 export function getOptions (lng = fallbackLng, ns: string | string[] = defaultNS) {
   return {
