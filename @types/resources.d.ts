810337c346c429ab14d1de367e59b92a2e862652
Joanna <jlapa.transport@gmail.com>
2024-01-08 14:45:49 +0100

    translation config fixed

--- @types/resources.d.ts
+++ @types/resources.d.ts
@@ -1,7 +1,10 @@
-interface Resources {
-  translation: {
-    title: string;
-    }
-  }
+import clientPage from '../app/i18n/locales/de/client-page.json';
+import translation from '../app/i18n/locales/de/translation.json';
+
+const resources = {
+  translation,
+  clientPage,
+  } as const;
+
   
-  export default Resources;
+  export default resources;
