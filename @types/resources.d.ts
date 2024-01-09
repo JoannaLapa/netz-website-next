dd99877321571cccbaa115ab41283c2e04aa7027
Joanna <jlapa.transport@gmail.com>
2024-01-09 13:59:25 +0100

    unneeded resources deleted

--- @types/resources.d.ts
+++ @types/resources.d.ts
@@ -1,9 +1,8 @@
-import clientPage from '../app/i18n/locales/de/client-page.json';
 import translation from '../app/i18n/locales/de/translation.json';
+
 
 const resources = {
   translation,
-  clientPage,
   } as const;
 
   
