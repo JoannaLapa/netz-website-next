1f4813400b7c70a68946b3fa17ca4775794e0d66
Joanna <jlapa.transport@gmail.com>
2024-01-12 10:32:04 +0100

    Wrapper inserted, styles added

--- app/components/sections/Header/component.tsx
+++ app/components/sections/Header/component.tsx
@@ -2,18 +2,21 @@ import * as React from 'react';
 import Image from 'next/image';
 import Navigation from '../Navigation/component';
 import { HeaderProps } from './component.types';
+import Wrapper from '../../ui/Wrapper';
 
 const Header: React.FC<HeaderProps> = (props) => {
   return (
-    <header className="flex w-full items-center justify-between lg:fixed lg:left-0 lg:top-0 lg:min-h-[80px] lg:bg-white">
-      <Image
-        alt="Company logo Netz with green net"
-        src="/assets/images/icon.png"
-        width={176}
-        height={65}
-      />
+    <header className="w-full lg:fixed lg:top-[60px] lg:min-h-[80px] lg:bg-white">
+      <Wrapper className="flex items-center justify-between">
+        <Image
+          alt="Company logo Netz with green net"
+          src="/assets/images/icon.png"
+          width={176}
+          height={65}
+        />
 
-      <Navigation lng={props.lng} />
+        <Navigation lng={props.lng} />
+      </Wrapper>
     </header>
   );
 };
