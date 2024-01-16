e3c170caeb97cd6ea0285a55f0b25798a1cf75e3
Joanna <jlapa.transport@gmail.com>
2024-01-16 11:33:51 +0100

    test added to button

--- app/components/ui/Button/component.test.tsx
+++ app/components/ui/Button/component.test.tsx
@@ -1,8 +1,17 @@
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import Button from './index';
 
 test('Render Button', () => {
   const element = render(<Button title="test" />);
 
   expect(element.getByText('test')).not.toBeNull();
+});
+
+test('Test Button onClick handler', () => {
+  const handleClick = jest.fn();
+
+  const { getByText } = render(<Button title="test" onClick={handleClick} />);
+
+  fireEvent.click(getByText('test'));
+  expect(handleClick).toHaveBeenCalled();
 });
