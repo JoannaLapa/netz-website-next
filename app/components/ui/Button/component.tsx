160f860dcc79e074b9c3ffbadfb712c1b5f4eda3
Joanna <jlapa.transport@gmail.com>
2024-01-16 10:58:52 +0100

    Link variant added, className moved to const for DRY

--- app/components/ui/Button/component.tsx
+++ app/components/ui/Button/component.tsx
@@ -1,9 +1,15 @@
 /* eslint-disable tailwindcss/no-custom-classname */
 import { ButtonProps } from './component.types';
+import Link from 'next/link';
 
+const className = "hover:border-primary-030  rounded-full border-2 border-primary-100 px-7 py-3 text-base font-semibold transition duration-300 focus:border-neutral-400"
 const Button: React.FC<ButtonProps> = (props) => {
+  if (props.link) {
+    return <Link href={props.link.href} className={className} aria-label={props.link.ariaLabel}>{props.title}</Link>;
+  }
+
   return (
-    <button className="border-primary-100  hover:border-primary-030 rounded-full border-2 px-7 py-3 text-base font-semibold transition duration-300 focus:border-neutral-400">
+    <button className={className} onClick={props.onClick} type={props.type}>
       {props.title}
     </button>
   );
