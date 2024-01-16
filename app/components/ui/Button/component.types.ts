1001f43fd5d3a74ef1a49a3e2b936c0d0d4e83f7
Joanna <jlapa.transport@gmail.com>
2024-01-16 11:36:16 +0100

    code format

--- app/components/ui/Button/component.types.ts
+++ app/components/ui/Button/component.types.ts
@@ -3,13 +3,13 @@ import { UrlObject } from 'url';
 type ButtonProps = {
   title: string;
   link?: LinkProps;
-  onClick?: React.MouseEventHandler<HTMLButtonElement>
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
   type?: 'submit' | 'button' | 'reset';
 };
 
 type LinkProps = {
   href: string | UrlObject;
   ariaLabel?: string;
-}
+};
 
 export type { ButtonProps };
