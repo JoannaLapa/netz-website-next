993ad082eccfdbc88df26420a044850796721f89
Joanna <jlapa.transport@gmail.com>
2024-01-16 10:58:23 +0100

    LinkProps added

--- app/components/ui/Button/component.types.ts
+++ app/components/ui/Button/component.types.ts
@@ -1,5 +1,15 @@
+import { UrlObject } from 'url';
+
 type ButtonProps = {
   title: string;
+  link?: LinkProps;
+  onClick?: React.MouseEventHandler<HTMLButtonElement>
+  type?: 'submit' | 'button' | 'reset';
 };
+
+type LinkProps = {
+  href: string | UrlObject;
+  ariaLabel?: string;
+}
 
 export type { ButtonProps };
