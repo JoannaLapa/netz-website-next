6a2c70735105f1a4eafbdfb268aee716461711c5
Joanna <jlapa.transport@gmail.com>
2024-01-22 10:34:43 +0100

    styles fixed

--- app/[lng]/page.tsx
+++ app/[lng]/page.tsx
@@ -1,7 +1,6 @@
 import { languages, fallbackLng } from '../i18n/settings';
 import { useTranslation } from '../i18n';
 import Header from '../components/sections/Header/index';
-import Button from '../components/ui/Button/index';
 import HeroSlider from '../components/sections/HeroSlider/index';
 
 export default async function Page({
@@ -17,7 +16,7 @@ export default async function Page({
   return (
     <>
       <Header lng={lng} />
-      <main className='mt-5 md:mt-[200px]'>
+      <main className='mt-5'>
         <HeroSlider headingTitle={t('title')} buttonTitle={t('askForQuote')} subtitle1={t('subtitle1')} subtitle2={t('subtitle2')} skipLink={t('sliderSkipLink')}/>
       </main>
     </>
