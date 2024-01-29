0fe23cc2b50613438900004ff880041eefcc3e1d
Joanna <jlapa.transport@gmail.com>
2024-01-29 12:42:07 +0100

    styles fixes for page constistency, code format

--- app/components/ui/Heading/component.tsx
+++ app/components/ui/Heading/component.tsx
@@ -2,7 +2,7 @@ import { HeadingProps } from './component.types';
 
 const Heading: React.FC<HeadingProps> = (props) => {
   return (
-    <h2 className=' text-lg font-light sm:text-2xl lg:text-[64px]'>
+    <h2 className='text-lg font-light sm:text-2xl lg:text-[64px]'>
       {props.title} <span className='font-medium'>{props.accentText}</span>
     </h2>
   );
