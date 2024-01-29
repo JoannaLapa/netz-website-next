90433b0745578a672fc3e50a64311873d3a7365d
Joanna <jlapa.transport@gmail.com>
2024-01-29 13:30:56 +0100

    styles fxes

--- app/components/sections/NumberedSection/NumberedBox/component.tsx
+++ app/components/sections/NumberedSection/NumberedBox/component.tsx
@@ -7,15 +7,15 @@ const NumberedBox: React.FC<NumberedBoxProps> = (props) => {
   const { t } = useTranslation();
 
   return (
-    <div className="relative min-h-[144px]">
-      <span className="font-robotoCondensed absolute left-0 top-0 -z-10 text-3xl font-bold text-neutral-200 opacity-80">
+    <div className="relative min-h-[144px] xl:flex xl:gap-2">
+      <span className="font-robotoCondensed absolute left-0 top-0 -z-10 text-3xl font-bold text-neutral-200 opacity-80 xl:static xl:leading-normal">
         {props.decorator}
       </span>
-      <div className="ml-[38px] mt-[42px]">
+      <div className="ml-[38px] mt-[42px] xl:ml-0 xl:mt-0">
         <h3 className=" font-robotoCondensed text-md font-bold text-neutral-400 lg:text-lg">
           {t(`numberedBoxes.${props.boxNr}.title`)}
         </h3>
-        <p className="text-base font-light text-neutral-300 lg:text-md">
+        <p className="lg:text-md text-base font-light text-neutral-300">
           {t(`numberedBoxes.${props.boxNr}.description`)}
         </p>
       </div>
