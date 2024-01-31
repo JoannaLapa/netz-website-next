d929ba570c3a5b5dfff31e6b147a58600fd95760
Joanna <jlapa.transport@gmail.com>
2024-01-31 08:45:02 +0100

    unworking head fixed

--- app/[lng]/layout.tsx
+++ app/[lng]/layout.tsx
@@ -1,7 +1,8 @@
 import { dir } from 'i18next';
 import { languages } from '../i18n/settings';
 import '../globals.css';
-import { Roboto_Slab, Roboto_Condensed } from 'next/font/google'
+import { Roboto_Slab, Roboto_Condensed } from 'next/font/google';
+import Head from './head';
 
 interface RootLayoutProps {
   children: React.ReactNode;
@@ -14,13 +15,13 @@ const roboto_condensed = Roboto_Condensed({
   subsets: ['latin'],
   display: 'swap',
   variable: '--font-roboto-condensed',
-})
- 
+});
+
 const roboto_slab = Roboto_Slab({
   subsets: ['latin'],
   display: 'swap',
   variable: '--font-roboto-slab',
-})
+});
 
 export async function generateStaticParams() {
   return languages.map((lng) => ({ lng }));
@@ -31,8 +32,12 @@ export default function RootLayout({
   params: { lng },
 }: RootLayoutProps) {
   return (
-    <html lang={lng} dir={dir(lng)} className={`${roboto_condensed.variable} ${roboto_slab.variable}`}>
-      <head />
+    <html
+      lang={lng}
+      dir={dir(lng)}
+      className={`${roboto_condensed.variable} ${roboto_slab.variable}`}
+    >
+      <Head params={{ lng }} />
       <body>{children}</body>
     </html>
   );
