d749ba8648a0d7c6072da5e62ef48a737a83f07f
Joanna <jlapa.transport@gmail.com>
2024-02-05 14:22:43 +0100

    wrapper styles modified

--- app/components/ui/Container/component.styles.ts
+++ app/components/ui/Container/component.styles.ts
@@ -4,7 +4,7 @@ import { twMerge } from 'tailwind-merge';
 const Container = cva('', {
   variants: {
     variant: {
-      section: 'mx-auto w-full max-w-full sm:max-w-[770px] xl:max-w-[1216px]',
+      section: 'mx-auto w-full max-w-full sm:max-w-[770px] md:max-w-[1216px]',
       sectionBig:
         'mx-auto w-full max-w-full sm:max-w-[976px] xl:max-w-[1520px]',
     },
