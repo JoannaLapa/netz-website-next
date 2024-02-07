0979502f6d5d79e35f2f5553aafc233c25ca2ae8
Joanna <jlapa.transport@gmail.com>
2024-02-07 18:36:08 +0100

    animation to ContentImageSection added

--- app/components/sections/NumberedSection/NumberedBox/component.tsx
+++ app/components/sections/NumberedSection/NumberedBox/component.tsx
@@ -1,18 +1,19 @@
-'use client';
+'use client'
+
 import { useTranslation } from 'react-i18next';
-import { motion } from 'framer-motion';
+// import { motion } from 'framer-motion';
 
 import { NumberedBoxProps } from './component.types';
 const NumberedBox: React.FC<NumberedBoxProps> = (props) => {
   const { t } = useTranslation();
 
   return (
-    <motion.div
+    <div
       className="relative min-h-[144px] scale-100 xl:flex xl:gap-2"
-      initial={{ opacity: 0, y: 100 }}
-      transition={{ duration: 1 }}
-      whileInView={{ opacity: 1, y: 0 }}
-      viewport={{ once: true, amount: 0.5 }}
+      // initial={{ opacity: 0, y: 100 }}
+      // transition={{ duration: 1 }}
+      // whileInView={{ opacity: 1, y: 0 }}
+      // viewport={{ once: true, amount: 0.5 }}
     >
       <span className="absolute left-0 top-0 -z-10 font-robotoCondensed text-3xl font-bold text-neutral-200 opacity-80 xl:static xl:leading-normal">
         {props.decorator}
@@ -25,7 +26,7 @@ const NumberedBox: React.FC<NumberedBoxProps> = (props) => {
           {t(`numberedBoxes.${props.boxNr}.description`)}
         </p>
       </div>
-    </motion.div>
+    </div>
   );
 };
 
