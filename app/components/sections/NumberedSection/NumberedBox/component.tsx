31cf5137cea6b7369a283bea8e7bdcbda282c3be
Joanna <jlapa.transport@gmail.com>
2024-02-07 18:37:55 +0100

    unneeded code deleted

--- app/components/sections/NumberedSection/NumberedBox/component.tsx
+++ app/components/sections/NumberedSection/NumberedBox/component.tsx
@@ -10,10 +10,6 @@ const NumberedBox: React.FC<NumberedBoxProps> = (props) => {
   return (
     <div
       className="relative min-h-[144px] scale-100 xl:flex xl:gap-2"
-      // initial={{ opacity: 0, y: 100 }}
-      // transition={{ duration: 1 }}
-      // whileInView={{ opacity: 1, y: 0 }}
-      // viewport={{ once: true, amount: 0.5 }}
     >
       <span className="absolute left-0 top-0 -z-10 font-robotoCondensed text-3xl font-bold text-neutral-200 opacity-80 xl:static xl:leading-normal">
         {props.decorator}
