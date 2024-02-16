e676bc6a9008aedd8437a196492d290d10d2f278
Joanna <jlapa.transport@gmail.com>
2024-02-16 14:14:50 +0100

    privacy policy page created

---
+++ app/[lng]/PrivacyPolicy/page.tsx
@@ -0,0 +1,309 @@
+import Heading from '@/app/components/ui/Heading';
+import Link from 'next/link';
+
+<>
+  <h1>PRIVACY POLICY</h1>
+  <p>Last updated February 16, 2024</p>
+  <p>
+    This privacy notice for Anita Surma und Michael Szczerbak Netz GbR ,
+    describes how and why we might collect, store, use, and/or share your
+    information when you use our services
+  </p>
+  <p>Services, such as when you:</p>
+  <p>
+    Visit our website{' '}
+    <a
+      href="https://netz-website-next.vercel.app"
+      target="_blank"
+      data-custom-class="link"
+    >
+      https://netz-website-next.vercel.app
+    </a>
+    , or any website of ours that links to this privacy notice
+  </p>
+  <Heading title="Questions or concerns?" /> 
+  <p>
+    Reading this privacy notice will help you understand your privacy rights and
+    choices. If you do not agree with our policies and practices, please do not
+    use our Services. If you still have any questions or concerns, please
+    contact us at anita.surma@direkt-netz.de.This summary provides key points
+    from our privacy notice. What personal information do we process? When you
+    visit, use, or navigate our Services, we may process personal information
+    depending on how you interact with us and the Services, the choices you
+    make, and the products and features you use. Learn more about personal
+    information you disclose to us
+  </p>
+  <Heading title="Do we process any sensitive personal information?" />
+  <p>We do not process sensitive personal information.</p>
+  <Heading title="Do we receive any information from third parties?" />
+  <p>We do not receive any information from third parties.</p>
+  <Heading title="How do we process your information?" />
+  <p>
+    We process your information to provide, improve, and administer our
+    Services, communicate with you, for security and fraud prevention, and to
+    comply with law. We may also process your information for other purposes
+    with your consent. We process your information only when we have a valid
+    legal reason to do so. In what situations and with which parties do we share
+    personal information? We may share information in specific situations and
+    with specific third parties. when and with whom we share your personal
+    information How do we keep your information safe? We have organisational and
+    technical processes and procedures in place to protect your personal
+    information. However, no electronic transmission over the internet or
+    information storage technology can be guaranteed to be 100% secure, so we
+    cannot promise or guarantee that hackers, cybercriminals, or other
+    unauthorised third parties will not be able to defeat our security and
+    improperly collect, access, steal, or modify your information.
+  </p>
+  <Heading title="What are your rights?" />
+  <p>
+    Depending on where you are located geographically, the applicable privacy
+    law may mean you have certain rights regarding your personal information.
+    How do you exercise your rights? The easiest way to exercise your rights is
+    by submitting a data subject access request, or by contacting us. We will
+    consider and act upon any request in accordance with applicable data
+    protection laws. Want to learn more about what we do with any information we
+    collect? 
+  </p>
+  <Heading title="1. WHAT INFORMATION DO WE COLLECT?" />
+  <p>
+    We collect personal information that you provide to us. We collect personal
+    information that you voluntarily provide to us when you express an interest
+    in obtaining information about us or our products and Services, when you
+    participate in activities on the Services, or otherwise when you contact us.
+    Personal Information Provided by You. The personal information that we
+    collect depends on the context of your interactions with us and the
+    Services, the choices you make, and the products and features you use. The
+    personal information we collect may include the following: contact or
+    authentication dataspanul mailing addresses company names Sensitive
+    Information. We do not process sensitive information. All personal
+    information that you provide to us must be true, complete, and accurate, and
+    you must notify us of any changes to such personal information.
+  </p>
+  <Heading title="2. HOW DO WE PROCESS YOUR INFORMATION?" />
+  <p>
+    In Short: We process your information to provide, improve, and administer
+    our Services, communicate with you, for security and fraud prevention, and
+    to comply with law. We may also process your information for other purposes
+    with your consent. We process your personal information for a variety of
+    reasons, depending on how you interact with our Services, including: To
+    deliver and facilitate delivery of services to the user. We may process your
+    information to provide you with the requested service. To respond to user
+    inquiries/offer support to users. We may process your information to respond
+    to your inquiries and solve any potential issues you might have with the
+    requested service. To send administrative information to you. We may process
+    your information to send you details about our products and services,
+    changes to our terms and policies, and other similar information. To fulfil
+    and manage your orders. We may process your information to fulfil and manage
+    your orders, payments, returns, and exchanges made through the Services. To
+    enable user-to-user communications. We may process your information if you
+    choose to use any of our offerings that allow for communication with another
+    user. To save or protect an individual&apos;s vital interest. We may process
+    your information when necessary to save or protect an individual’s vital
+    interest, such as to prevent harm
+  </p>
+  <Heading title="3. WHAT LEGAL BASES DO WE RELY ON TO PROCESS YOUR INFORMATION?" />
+  <p>
+    We only process your personal information when we believe it is necessary
+    and we have a valid legal reason (i.e. legal basis) to do so under
+    applicable law, like with your consent, to comply with laws, to provide you
+    with services to enter into or fulfil our contractual obligations, to
+    protect your rights, or to fulfil our legitimate business interests. The
+    General Data Protection Regulation (GDPR) and UK GDPR require us to explain
+    the valid legal bases we rely on in order to process your personal
+    information. As such, we may rely on the following legal bases to process
+    your personal information: Consent. We may process your information if you
+    have given us permission (i.e.consent) to use your personal information for
+    a specific purpose. You can withdraw your consent at any time. Learn more
+    about Performance of a Contract. We may process your personal information
+    when we believe it is necessary to fulfil our contractual obligations to
+    you, including providing our Services or at your request prior to entering
+    into a contract with you. We may process your information where we believe
+    it is necessary for compliance with our legal obligations, such as to
+    cooperate with a law enforcement body or regulatory agency, exercise or
+    defend our legal rights, or disclose your information as evidence in
+    litigation in which we are involved. Vital Interests. We may process your
+    information where we believe it is necessary to protect your vital interests
+    or the vital interests of a third party, such as situations involving
+    potential threats to the safety of any person.
+  </p>
+  <Heading title="4. WHEN AND WITH WHOM DO WE SHARE YOUR PERSONAL INFORMATION?" />
+  <p>
+    In Short: We may share information in specific situations described in this
+    section and/or with the following third parties. Vendors, Consultants, and
+    Other Third-Party Service Providers. We may share your data with third-party
+    vendors, service providers, contractors, or agents (&apos;third
+    parties&apos;) who perform services for us or on our behalf and require
+    access to such information to do that work. We have contracts in place with
+    our third parties, which are designed to help safeguard your personal
+    information. This means that they cannot do anything with your personal
+    information unless we have instructed them to do it. They will also not
+    share your personal information with any organization apart from us. They
+    also commit to protect the data they hold on our behalf and to retain it for
+    the period we instruct. The third parties we may share personal information
+    with are as follows: Communicate and Chat with Users EmailJS Pte. Ltd.
+    Website Hosting cyber_Folks S.A. We also may need to share your personal
+    information in the following situations: Business Transfers. We may share or
+    transfer your information in connection with, or during negotiations of, any
+    merger, sale of company assets, financing, or acquisition of all or a
+    portion of our business to another company.
+  </p>
+  <Heading title="DO WE USE COOKIES AND OTHER TRACKING TECHNOLOGIES?" />
+  <p>
+    In Short: We may use cookies and other tracking technologies to collect and
+    store your information. We may use cookies and similar tracking technologies
+    (like web beacons and pixels) to access or store information. Specific
+    information about how we use such technologies and how you can refuse
+    certain cookies is set out in our Cookie Notice:
+    https://app.termly.io/dashboard/website/3035073/cookie-policy.
+  </p>
+  <Heading title=" 6. IS YOUR INFORMATION TRANSFERRED INTERNATIONALLY?" />
+  <p>
+    We may transfer, store, and process your information in countries other than
+    your own. Our servers are located in If you are accessing our Services from
+    outside, please be aware that your information may be transferred to,
+    stored, and processed by us in our facilities and by those third parties
+    with whom we may share your personal information (see &apos;WHEN AND WITH
+    WHOM DO WE SHARE YOUR PERSONAL INFORMATION?&apos; above), in the United
+    States and other countries. If you are a resident in the European Economic
+    Area (EEA), United Kingdom (UK), or Switzerland, then these countries may
+    not necessarily have data protection laws or other similar laws as
+    comprehensive as those in your country. However, we will take all necessary
+    measures to protect your personal information in accordance with this
+    privacy notice and applicable law. European Commission&apos;s Standard
+    Contractual Clauses: We have implemented measures to protect your personal
+    information, including by using the European Commission&apos;s Standard
+    Contractual Clauses for transfers of personal information between our group
+    companies and between us and our third-party providers. These clauses
+    require all recipients to protect all personal information that they process
+    originating from the EEA or UK in accordance with European data protection
+    laws and regulations. Our Standard Contractual Clauses can be provided upon
+    request. We have implemented similar appropriate safeguards with our
+    third-party service providers and partners and further details can be
+    provided upon request.
+  </p>
+  <Heading title="7. HOW LONG DO WE KEEP YOUR INFORMATION?" />
+  <p>
+    In Short: We keep your information for as long as necessary to fulfill the
+    purposes outlined in this privacy notice unless otherwise required by law.
+    We will only keep your personal information for as long as it is necessary
+    for the purposes set out in this privacy notice, unless a longer retention
+    period is required or permitted by law (such as tax, accounting, or other
+    legal requirements). When we have no ongoing legitimate business need to
+    process your personal information, we will either delete or anonymize such
+    information, or, if this is not possible (for example, because your personal
+    information has been stored in backup archives), then we will securely store
+    your personal information and isolate it from any further processing until
+    deletion is possible.
+  </p>
+  <Heading title="8. HOW DO WE KEEP YOUR INFORMATION SAFE?" />
+  <p>
+    In Short: We aim to protect your personal information through a system of
+    organizational and technical security measures. We have implemented
+    appropriate and reasonable technical and organizational security measures
+    designed to protect the security of any personal information we process.
+    However, despite our safeguards and efforts to secure your information, no
+    electronic transmission over the Internet or information storage technology
+    can be guaranteed to be 100% secure, so we cannot promise or guarantee that
+    hackers, cybercriminals, or other unauthorized third parties will not be
+    able to defeat our security and improperly collect, access, steal, or modify
+    your information. Although we will do our best to protect your personal
+    information, transmission of personal information to and from our Services
+    is at your own risk. You should only access the Services within a secure
+    environment.
+  </p>
+  <Heading title=" 9. DO WE COLLECT INFORMATION FROM MINORS?" />
+  <p>
+    In Short: We do not knowingly collect data from or market to children under
+    18 years of age. We do not knowingly solicit data from or market to children
+    under 18 years of age. By using the Services, you represent that you are at
+    least 18 or that you are the parent or guardian of such a minor and consent
+    to such minor dependent’s use of the Services. If we learn that personal
+    information from users less than 18 years of age has been collected, we will
+    deactivate the account and take reasonable measures to promptly delete such
+    data from our records. If you become aware of any data we may have collected
+    from children under age 18, please contact us at anita.surma@direkt-netz.de.
+  </p>
+  <Heading title="10. WHAT ARE YOUR PRIVACY RIGHTS?" />
+  <p>
+    In Short: In some regions, such as the European Economic Area (EEA), United
+    Kingdom (UK), and Switzerland, you have rights that allow you greater access
+    to and control over your personal information. You may review, change, or
+    terminate your account at any time. In some regions (like the EEA, UK, and
+    Switzerland), you have certain rights under applicable data protection laws.
+    These may include the right (i) to request access and obtain a copy of your
+    personal information, (ii) to request rectification or erasure; (iii) to
+    restrict the processing of your personal information; (iv) if applicable, to
+    data portability; and (v) not to be subject to automated decision-making. In
+    certain circumstances, you may also have the right to object to the
+    processing of your personal information. You can make such a request by
+    contacting us by using the contact details provided in the section &apos;HOW
+    CAN YOU CONTACT US ABOUT THIS NOTICE?&apos; below. We will consider and act
+    upon any request in accordance with applicable data protection laws. If you
+    are located in the EEA or UK and you believe we are unlawfully processing
+    your personal information, you also have the right to complain to your
+    Member State data protection authority or UK data protection authority. If
+    you are located in Switzerland, you may contact the Federal Data Protection
+    and Information Commissioner. Withdrawing your consent: If we are relying on
+    your consent to process your personal information, you have the right to
+    withdraw your consent at any time. You can withdraw your consent at any time
+    by contacting us by using the contact details provided in the section
+    &apos;HOW CAN YOU CONTACT US ABOUT THIS NOTICE?&apos; below. However, please
+    note that this will not affect the lawfulness of the processing before its
+    withdrawal nor, will it affect the processing of your personal information
+    conducted in reliance on lawful processing grounds other than consent.
+    Cookies and similar technologies: Most Web browsers are set to accept
+    cookies by default. If you prefer, you can usually choose to set your
+    browser to remove cookies and to reject cookies. If you choose to remove
+    cookies or reject cookies, this could affect certain features or services of
+    our Services. For further information, please see our Cookie Notice:
+    https://app.termly.io/dashboard/website/3035073/cookie-policy. If you have
+    questions or comments about your privacy rights, you may email us at
+    anita.surma@direkt-netz.de.
+    <Heading title="11. Controls for Do-Not-Track Features" />
+    <p>
+      Most web browsers and some mobile operating systems and mobile
+      applications include a Do-Not-Track (DNT) feature or setting you can
+      activate to signal your privacy preference not to have data about your
+      online browsing activities monitored and collected. At this stage no
+      uniform technology standard for recognizing and implementing DNT signals
+      has been finalized. As such, we do not currently respond to DNT browser
+      signals or any other mechanism that automatically communicates your choice
+      not to be tracked online. If a standard for online tracking is adopted
+      that we must follow in the future, we will inform you about that practice
+      in a revised version of this privacy notice.
+    </p>
+    <Heading title="12. Do we make updates to this notice?" />
+    <p>
+      Yes, we will update this notice as necessary to stay compliant with
+      relevant laws. We may update this privacy notice from time to time. The
+      updated version will be indicated by an updated &apos;Revised&apos; date
+      and the updated version will be effective as soon as it is accessible. If
+      we make material changes to this privacy notice, we may notify you either
+      by prominently posting a notice of such changes or by directly sending you
+      a notification. We encourage you to review this privacy notice frequently
+      to be informed of how we are protecting your information.{' '}
+    </p>
+    <Heading title="How can you contact us about this notice?" />
+    <p>
+      If you have questions or comments about this notice, you may email us at
+      anita.surma@direkt-netz.de or contact us by post at:
+    </p>
+  </p>
+  <address>
+    Anita Surma und Michael Szczerbak Netz GbR Im Weimer 8, 61137 Schöneck,
+    Germany Schöneck, Hessen, 61137 Germany
+  </address>
+  <Heading title="How can you review, update, or delete the data we collect from you?" />
+  <p>
+    You have the right to request access to the personal information we collect
+    from you, change that information or delete it. You can contact us at
+    anita.surma@direkt-netz.de to request to review, update or delete your
+    personal information.
+  </p>
+  <p>
+    The Privacy Policy was created by{' '}
+    <Link href="https://termly.io/products/privacy-policy-generator/">
+      Termly Privacy Policy Generator
+    </Link>
+  </p>
+</>;
