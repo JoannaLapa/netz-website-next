ca3aa4877f4b0e3d56cbeaa71711e7cb6c53ffe7
Joanna <jlapa.transport@gmail.com>
2024-02-19 14:44:49 +0100

    privacyPolicy translation file added

--- @types/resources.d.ts
+++ @types/resources.d.ts
@@ -1,7 +1,8 @@
 import translation from '../app/i18n/locales/de/translation.json';
-
+import privacyPoliy from '../app/i18n/locales/de/privacyPolicy.json';
 const resources = {
   translation,
+  privactyPolicy,
 } as const;
 
 export default resources;
