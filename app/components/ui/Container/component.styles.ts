544011fdf7781522e8242d5f7e297f2eea973f54
Joanna <jlapa.transport@gmail.com>
2024-02-21 15:53:02 +0100

    sectionSmall variant to container added

--- app/components/ui/Container/component.styles.ts
+++ app/components/ui/Container/component.styles.ts
@@ -4,6 +4,8 @@ import { twMerge } from 'tailwind-merge';
 const Container = cva('', {
   variants: {
     variant: {
+      sectionSmall:
+        'mx-auto w-full max-w-full sm:max-w-[640px] md:max-w-[800px]',
       section: 'mx-auto w-full max-w-full sm:max-w-[770px] md:max-w-[1216px]',
       sectionBig:
         'mx-auto w-full max-w-full sm:max-w-[976px] xl:max-w-[1520px]',
