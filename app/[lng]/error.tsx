e382c3bce315adb82f5f4695929fb557fb139f91
Joanna <jlapa.transport@gmail.com>
2024-03-06 12:33:17 +0100

    error and not-found page finished

--- app/[lng]/error.tsx
+++ app/[lng]/error.tsx
@@ -19,24 +19,33 @@ export default function Error({
 
   return (
     <main>
-      <Wrapper element="div" variant="section" className="mb-12 xl:mb-0">
-        <Container variant="section">
-          <div className="flex justify-center rounded-xl rounded-br-none border-solid border-neutral-200 bg-black px-6 pb-[64px] pt-[40px] md:pb-[128px] md:pt-[88px]">
-            <div className="flex w-full max-w-[800px] flex-col items-center gap-6 text-center md:gap-12">
-              <h1 className="text-xl text-white">Whoops, That Page Is Gone!</h1>
+      <Wrapper
+        element="div"
+        variant="section"
+        className="mb-12 flex h-screen justify-center xl:mb-0"
+      >
+        <Container
+          variant="section"
+          className="flex flex-col items-center justify-center"
+        >
+          <div className="flex w-full max-w-[800px] flex-col items-center gap-6 text-center md:gap-12">
+            <h1 className="text-2xl">Oops, something went wrong!</h1>
 
-              <p>
-                We’re so sorry, this link is broken or the page may have been
-                removed.
-              </p>
-              <div className="flex flex-col items-center justify-center md:flex-row">
-                <Button
-                  variant="primary"
-                  onClick={() => reset()}
-                  title="Try again"
-                />
-                <Link href="/">GO TO HOMEPAGE</Link>
-              </div>
+            <p>
+              We apologize, but an unexpected error occurred. Please try again
+              later.
+            </p>
+            <div className="flex flex-col items-center justify-center gap-8 md:flex-row">
+              <Button
+                variant="primary"
+                onClick={() => reset()}
+                title="Try again"
+              />
+              <Button
+                variant="primary"
+                link={{ href: '/' }}
+                title="GO TO HOMEPAGE"
+              />
             </div>
           </div>
         </Container>
--- app/[lng]/page.tsx
+++ app/[lng]/page.tsx
@@ -1,6 +1,5 @@
 import { languages, fallbackLng } from '../i18n/settings';
 import { useTranslation } from '../i18n';
-import { notFound } from 'next/navigation';
 import Header from '../components/sections/Header/index';
 import HeroSlider from '../components/sections/HeroSlider/index';
 import BoxImageSection from '../components/sections/BoxImageSection';
@@ -28,10 +27,6 @@ export default async function Page({
 }) {
   if (languages.indexOf(lng) < 0) lng = fallbackLng;
   const { t, i18n } = await useTranslation(lng);
-
-  if (!Page) {
-    return notFound();
-  }
 
   return (
     <>
