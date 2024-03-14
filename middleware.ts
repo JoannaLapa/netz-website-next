04d910a62a0ce6b3c48374536dc20af4ebebde10
Joanna <jlapa.transport@gmail.com>
2024-03-14 10:48:18 +0100

    csp headers removed as unneeded

--- middleware.ts
+++ middleware.ts
@@ -5,17 +5,8 @@ import { fallbackLng, languages, cookieName } from './app/i18n/settings';
 acceptLanguage.languages(languages);
 
 export const config = {
-  matcher: [
-    {
-      source: '/((?!api|_next/static|_next/image|assets|favicon.ico).*)',
-      missing: [
-        { type: 'header', key: 'next-router-prefetch' },
-        { type: 'header', key: 'purpose', value: 'prefetch' },
-      ],
-    },
-  ],
+  matcher: ['/((?!api|_next/static|_next/image|assets|favicon.ico|sw.js).*)'],
 };
-
 export function middleware(req: NextRequest) {
   if (
     req.nextUrl.pathname.indexOf('icon') > -1 ||
@@ -23,13 +14,10 @@ export function middleware(req: NextRequest) {
   )
     return NextResponse.next();
   let lng: string | undefined | null;
-
   if (req.cookies.has(cookieName))
     lng = acceptLanguage.get(req.cookies.get(cookieName)?.value);
-
   if (!lng) lng = acceptLanguage.get(req.headers.get('Accept-Language'));
   if (!lng) lng = fallbackLng;
-
   // Redirect if lng in path is not supported
   if (
     !languages.some((loc) => req.nextUrl.pathname.startsWith(`/${loc}`)) &&
@@ -45,38 +33,7 @@ export function middleware(req: NextRequest) {
     const lngInReferer = languages.find((l) =>
       refererUrl.pathname.startsWith(`/${l}`),
     );
-
-    const nonce = Buffer.from(crypto.randomUUID()).toString('base64');
-    const cspHeader = `
-      default-src 'self';
-      script-src 'self' 'nonce-${nonce}' 'strict-dynamic';
-      style-src 'self' 'nonce-${nonce}';
-      img-src 'self' blob: data:;
-      font-src 'self';
-      object-src 'none';
-      base-uri 'self';
-      form-action 'self';
-      frame-ancestors 'none';
-      block-all-mixed-content;
-      upgrade-insecure-requests;
-  `;
-    // Replace newline characters and spaces
-    const contentSecurityPolicyHeaderValue = cspHeader
-      .replace(/\s{2,}/g, ' ')
-      .trim();
-
-    const requestHeaders = new Headers(req.headers);
-    requestHeaders.set('x-nonce', nonce);
-
-    requestHeaders.set(
-      'Content-Security-Policy',
-      contentSecurityPolicyHeaderValue,
-    );
-    const response = NextResponse.next({
-      request: {
-        headers: requestHeaders,
-      },
-    });
+    const response = NextResponse.next();
     if (
       req.cookies.has('cookieConsent') &&
       req.cookies.get('cookieConsent')?.value === 'accepted' &&
@@ -84,15 +41,8 @@ export function middleware(req: NextRequest) {
     ) {
       response.cookies.set(cookieName, lngInReferer);
     }
-
-    // Set the Content-Security-Policy header
-    response.headers.set(
-      'Content-Security-Policy',
-      contentSecurityPolicyHeaderValue,
-    );
-
     return response;
   }
-
   return NextResponse.next();
 }
+
